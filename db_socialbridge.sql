b2a423966581dbd00b85866c5a9d9582def2433c
suraj-k-s <surajks28101999@gmail.com>
2024-03-14 22:13:55 +0530

    first commit

---
+++ db_socialbridge.sql
@@ -0,0 +1,348 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2024 at 05:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_socialbridge`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL,
+  `admin_email` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_chat`
+--
+
+CREATE TABLE `tbl_chat` (
+  `chat_id` int(11) NOT NULL,
+  `chat_content` varchar(300) NOT NULL,
+  `influencer_from_id` int(11) NOT NULL DEFAULT 0,
+  `influencer_to_id` int(11) NOT NULL DEFAULT 0,
+  `sponser_from_id` int(11) NOT NULL DEFAULT 0,
+  `sponser_to_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_complaint`
+--
+
+CREATE TABLE `tbl_complaint` (
+  `complaint_id` int(11) NOT NULL,
+  `complaint_date` varchar(100) NOT NULL,
+  `complaint_details` varchar(100) NOT NULL,
+  `complaint_reply` varchar(100) NOT NULL DEFAULT 'Not Yet Replied',
+  `complaint_status` int(11) NOT NULL DEFAULT 0,
+  `influencer_id` int(11) NOT NULL,
+  `complaint_reply_date` varchar(10) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_district`
+--
+
+CREATE TABLE `tbl_district` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_influencer`
+--
+
+CREATE TABLE `tbl_influencer` (
+  `influencer_id` int(11) NOT NULL,
+  `influencer_name` varchar(100) NOT NULL,
+  `influencer_email` varchar(100) NOT NULL,
+  `influencer_address` varchar(100) NOT NULL,
+  `influencer_photo` varchar(100) NOT NULL,
+  `influencer_proof` varchar(100) NOT NULL,
+  `influencer_password` varchar(100) NOT NULL,
+  `influencer_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_place`
+--
+
+CREATE TABLE `tbl_place` (
+  `place_id` int(11) NOT NULL,
+  `place_name` varchar(100) NOT NULL,
+  `district_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_post`
+--
+
+CREATE TABLE `tbl_post` (
+  `post_id` int(11) NOT NULL,
+  `post_title` varchar(100) NOT NULL,
+  `post_details` varchar(100) NOT NULL,
+  `post_photo` varchar(100) NOT NULL,
+  `subcategory_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_request`
+--
+
+CREATE TABLE `tbl_request` (
+  `request_id` int(11) NOT NULL,
+  `request_date` varchar(100) NOT NULL,
+  `request_details` varchar(100) NOT NULL,
+  `request_status` int(11) NOT NULL DEFAULT 0,
+  `sponser_id` int(11) NOT NULL,
+  `influncer_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_review`
+--
+
+CREATE TABLE `tbl_review` (
+  `review_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_rating` varchar(100) NOT NULL,
+  `user_review` varchar(100) NOT NULL,
+  `review_datetime` varchar(100) NOT NULL,
+  `sponser_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sponser`
+--
+
+CREATE TABLE `tbl_sponser` (
+  `sponser_id` int(11) NOT NULL,
+  `sponser_name` varchar(100) NOT NULL,
+  `sponser_contact` varchar(100) NOT NULL,
+  `sponser_email` varchar(100) NOT NULL,
+  `sponser_address` varchar(100) NOT NULL,
+  `sponser_photo` varchar(100) NOT NULL,
+  `sponser_proof` varchar(100) NOT NULL,
+  `sponser_password` varchar(100) NOT NULL,
+  `sponser_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_subcategory`
+--
+
+CREATE TABLE `tbl_subcategory` (
+  `subcategory_id` int(11) NOT NULL,
+  `subcategory_name` varchar(100) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `tbl_chat`
+--
+ALTER TABLE `tbl_chat`
+  ADD PRIMARY KEY (`chat_id`);
+
+--
+-- Indexes for table `tbl_complaint`
+--
+ALTER TABLE `tbl_complaint`
+  ADD PRIMARY KEY (`complaint_id`);
+
+--
+-- Indexes for table `tbl_district`
+--
+ALTER TABLE `tbl_district`
+  ADD PRIMARY KEY (`district_id`);
+
+--
+-- Indexes for table `tbl_influencer`
+--
+ALTER TABLE `tbl_influencer`
+  ADD PRIMARY KEY (`influencer_id`);
+
+--
+-- Indexes for table `tbl_place`
+--
+ALTER TABLE `tbl_place`
+  ADD PRIMARY KEY (`place_id`);
+
+--
+-- Indexes for table `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `tbl_request`
+--
+ALTER TABLE `tbl_request`
+  ADD PRIMARY KEY (`request_id`);
+
+--
+-- Indexes for table `tbl_review`
+--
+ALTER TABLE `tbl_review`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- Indexes for table `tbl_sponser`
+--
+ALTER TABLE `tbl_sponser`
+  ADD PRIMARY KEY (`sponser_id`);
+
+--
+-- Indexes for table `tbl_subcategory`
+--
+ALTER TABLE `tbl_subcategory`
+  ADD PRIMARY KEY (`subcategory_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_chat`
+--
+ALTER TABLE `tbl_chat`
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_complaint`
+--
+ALTER TABLE `tbl_complaint`
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_district`
+--
+ALTER TABLE `tbl_district`
+  MODIFY `district_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_influencer`
+--
+ALTER TABLE `tbl_influencer`
+  MODIFY `influencer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_place`
+--
+ALTER TABLE `tbl_place`
+  MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_post`
+--
+ALTER TABLE `tbl_post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_request`
+--
+ALTER TABLE `tbl_request`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_review`
+--
+ALTER TABLE `tbl_review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_sponser`
+--
+ALTER TABLE `tbl_sponser`
+  MODIFY `sponser_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_subcategory`
+--
+ALTER TABLE `tbl_subcategory`
+  MODIFY `subcategory_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
